00172e2b128efaaf7bd3c7d71f49c6ad9069bda7
Juan M. Triguero <juan.triguero@salesforce.com>
2025-10-13 16:20:44 +0200

    Import and export

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,8 +1,11 @@
 import { Status } from '@/components/Screen';
 import { SavedItem } from '@/models/interfaces';
-import { Realm, useQuery } from '@realm/react';
+import { Realm, useQuery, useRealm } from '@realm/react';
+import * as DocumentPicker from 'expo-document-picker';
+import { File, Paths } from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 import { SymbolView } from 'expo-symbols';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Pressable, PlatformColor } from 'react-native';
 
 type Props = {
     schema: Realm.ObjectClass<SavedItem & Realm.Object>,
@@ -12,6 +15,7 @@ type Props = {
 export default function Stats({ schema, statusOptions }: Props) {
 
     const items = useQuery(schema);
+    const realm = useRealm();
 
     const renderStat = ({ item }: { item: Status }) => {
         const count = items.filtered(`status == $0`, item.value).length;
@@ -26,13 +30,49 @@ export default function Stats({ schema, statusOptions }: Props) {
         );
     };
 
+    const importItems = () => {
+        DocumentPicker.getDocumentAsync({ type: 'application/json' })
+        .then(({ assets }) => {
+            if (assets?.length) {
+                const file = new File(assets[0].uri);
+                const items = JSON.parse(file.textSync());
+                realm.write(() => {
+                    items.forEach((item: SavedItem) => {
+                        realm.create(schema, item, Realm.UpdateMode.Modified);
+                    });
+                });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert('There was an error during the import, please use a valid file');
+        });
+    };
+
+    const exportItems = () => {
+        const file = new File(Paths.cache, `${schema.name}_${Date.now()}.json`);
+        file.create();
+        file.write(JSON.stringify(items));
+        Sharing.shareAsync(file.uri);
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text style={styles.headerTitle}>Statistics</Text>
+                <Text style={styles.headerTitle}>My collection</Text>
             </View>
             <View style={styles.stats}>
                 <FlatList data={statusOptions} renderItem={renderStat} />
+            </View>
+            <View style={styles.actions}>
+                <Pressable onPress={importItems} style={styles.action}>
+                    <SymbolView name="document.badge.plus" size={35} />
+                    <Text style={styles.actionText}>Import</Text>
+                </Pressable>
+                <Pressable onPress={exportItems} style={styles.action}>
+                <SymbolView name="document.badge.arrow.up" size={35} />
+                    <Text style={styles.actionText}>Export</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -40,6 +80,25 @@ export default function Stats({ schema, statusOptions }: Props) {
 }
 
 const styles = StyleSheet.create({
+    actions: {
+        flexDirection: 'row',
+        gap: 10,
+        marginVertical: 10,
+    },
+    action: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        borderRadius: 15,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        padding: 15,
+    },
+    actionText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: PlatformColor('systemBlue'),
+    },
     container: {
         margin: 10,
     },
